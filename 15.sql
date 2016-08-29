6527d2960aff9f7b844791580493255923a2b227
MATT BRUTON <the.bruton@gmail.com>
2016-08-29 17:21:20 -0500

    query 15

---
+++ 15.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT Playlist.Name AS 'Playlist Name', COUNT(PlaylistTrack.TrackId) AS '# of Tracks' FROM Playlist JOIN PlaylistTrack ON Playlist.PlaylistId == PlaylistTrack.PlaylistId GROUP BY PlaylistTrack.PlaylistId
